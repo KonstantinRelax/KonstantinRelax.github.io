b4705884df2ef4901806b25a8956a7624b5bda0b
KonstantinRelax <monologic.loggy@yandex.com>
2017-01-28 14:56:50 +0600

    second commit

--- php/mail.php
+++ php/mail.php
@@ -1,20 +1,20 @@
 <?php
 //Принимаем постовые данные
 $uname=$_POST['uname'];
-$email=$_POST['email'];
+$phone=$_POST['phone'];
 $msg=$_POST['msg'];
 //Тут указываем на какой ящик посылать письмо
 $to = "monologic.loggy@yandex.com";
 //Далее идет тема и само сообщение
 // Тема письма
-$subject = "Заявка с сайта";
+$subject = "Отзыв с сайта";
 // Сообщение письма
 $message = "
 Имя пользователя: ".htmlspecialchars($uname)."<br />
-Email: ".htmlspecialchars($email)."<br />
+Телефон: ".htmlspecialchars($phone)."<br />
 Сообщение: ".$msg;
 // Отправляем письмо при помощи функции mail();
-$headers = "From: lessons <hzgde@stastroi.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
+$headers = "From: Homepage <hzgde@stastroi.ru>\r\nContent-type: text/html; charset=UTF-8 \r\n";
 mail ($to, $subject, $message, $headers);
 // Перенаправляем человека на страницу благодарности и завершаем скрипт
 header('Location: ..//thanks.html');
